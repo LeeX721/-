b46810698fbd174295b4361ed834343319dfbe9d
lixiang <2744185951@qq.com>
2024-07-28 21:36:33 +0800

    fix: Initiel commit: Add project files and configurations

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>App跟组件</h1>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { reqLogin } from './api/user'
+onMounted(() => {
+  reqLogin({ username: 'admin', password: '111111' })
+})
+</script>
+<style></style>
